080dcefef88069cc21220dc31df993f391a1460d
marckr <decariamarco98@gmail.com>
2024-01-31 20:55:38 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -44,8 +44,63 @@ STATE_CODES_MAPPING = {'AL': 'Alabama', 'AK': 'Alaska', 'AZ': 'Arizona', 'AR': '
                        'SD': 'South Dakota', 'TN': 'Tennessee', 'TX': 'Texas', 'UT': 'Utah', 'VT': 'Vermont',
                        'VA': 'Virginia', 'WA': 'Washington', 'WV': 'West Virginia', 'WI': 'Wisconsin', 'WY': 'Wyoming'}
 
+ELECTORAL_VOTES = {
+    'Alabama': 9,
+    'Alaska': 3,
+    'Arizona': 11,
+    'Arkansas': 6,
+    'California': 55,
+    'Colorado': 9,
+    'Connecticut': 7,
+    'Delaware': 3,
+    'Florida': 29,
+    'Georgia': 16,
+    'Hawaii': 4,
+    'Idaho': 4,
+    'Illinois': 20,
+    'Indiana': 11,
+    'Iowa': 6,
+    'Kansas': 6,
+    'Kentucky': 8,
+    'Louisiana': 8,
+    'Maine': 4,
+    'Maryland': 10,
+    'Massachusetts': 11,
+    'Michigan': 16,
+    'Minnesota': 10,
+    'Mississippi': 6,
+    'Missouri': 10,
+    'Montana': 3,
+    'Nebraska': 5,
+    'Nevada': 6,
+    'New Hampshire': 4,
+    'New Jersey': 14,
+    'New Mexico': 5,
+    'New York': 29,
+    'North Carolina': 15,
+    'North Dakota': 3,
+    'Ohio': 18,
+    'Oklahoma': 7,
+    'Oregon': 7,
+    'Pennsylvania': 20,
+    'Rhode Island': 4,
+    'South Carolina': 9,
+    'South Dakota': 3,
+    'Tennessee': 11,
+    'Texas': 38,
+    'Utah': 6,
+    'Vermont': 3,
+    'Virginia': 13,
+    'Washington': 12,
+    'West Virginia': 5,
+    'Wisconsin': 10,
+    'Wyoming': 3,
+    'District of Columbia': 3
+}
+
+
 COLUMNS = ["tweet_id", "created_at", "user_id_str", "text", "hashtags", "retweet_count", "favorite_count",
            "in_reply_to_screen_name", "source", "retweeted", "lang", "location", "screen_name"]
 
-DEVICES = {"iphone": "Twitter for iPhone", "android": "Twitter for Android", "ipad": "Twitter for iPad",
-           "webapp": "Twitter Web App"}
+DAY_START = 1
+DAY_END = 31
