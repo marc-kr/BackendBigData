ff9a6a4c7174b898dd8cc3d5babe2e1cf63a669d
marckr <decariamarco98@gmail.com>
2024-02-03 18:04:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask
 import logging
 
 from pyspark.sql import SparkSession
@@ -6,60 +6,14 @@ from dataset import *
 
 from spark_repository import SparkRepository
 
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
-spark = SparkSession.builder.config("spark.driver.bindAddress", "127.0.0.1").getOrCreate()
+spark = SparkSession.builder.config("spark.driver.bindAddress", "127.0.0.1").config('spark.driver.memory', '2g').getOrCreate()
+dataset = Dataset(spark)
+spark_repository = SparkRepository(dataset.twitter_data)
 
-spark_repository = SparkRepository(get_twitter_data(spark))
-
-
-@app.route("/devices/count")
-def devices_count():
-    return spark_repository.devices_count()
-
-
-@app.route("/tweets/most_retweeted")
-def most_retweeted():
-    limit = int(request.args.get('limit', default=10))
-    return spark_repository.most_retweeted(limit)
-
-
-@app.route("/tweets/total_count")
-def total_tweets():
-    return f"{spark_repository.total_tweets_count()}"
-
-
-@app.route("/tweets/frequency/daily")
-def tweets_frequency_daily():
-    return spark_repository.tweet_frequency_daily()
-
-
-@app.route("/tweets/frequency/hourly")
-def tweets_frequency_hourly():
-    return spark_repository.tweet_frequency_hourly()
-
-
-@app.route("/tweets/by_lang/count")
-def tweets_lang_count():
-    return spark_repository.tweet_lang_count()
-
-
-@app.route("/tweets/daily_avg")
-def tweets_daily_avg():
-    return f"{spark_repository.tweets_daily_avg()}"
-
-
-@app.route("/hashtags/total_count")
-def hashtags_total_count():
-    return f"{spark_repository.hashtags_total_count()}"
-
-
-@app.route("/hashtags/most_popular")
-def most_popular_hashtags():
-    limit = int(request.args.get('limit', default=10))
-    return spark_repository.most_popular_hashtags(limit)
-
+import controllers
 
 if __name__ == '__main__':
     app.run(debug=True)
