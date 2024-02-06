b95ce95462cfe2dd897de881e443064b3c79ca4d
marckr <decariamarco98@gmail.com>
2024-02-06 21:48:19 +0100

    Loading trump and biden tweets

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from spark_repository import SparkRepository
 #logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
-spark = SparkSession.builder.config("spark.driver.bindAddress", "127.0.0.1").config('spark.driver.memory', '2g').getOrCreate()
+spark = SparkSession.builder.config("spark.driver.bindAddress", "127.0.0.1").config('spark.driver.memory', '3g').config('spark.executor.memory', '2g').getOrCreate()
 dataset = Dataset(spark)
-spark_repository = SparkRepository(dataset.twitter_data)
+spark_repository = SparkRepository(dataset.twitter_data, dataset.trump_tweets, dataset.biden_tweets)
 
 import controllers
 
--- dataset.py
+++ dataset.py
@@ -17,7 +17,11 @@ DAY_END = 31
 class Dataset:
     def __init__(self, spark):
         self.twitter_data = get_twitter_data(spark)
+        self.trump_tweets = get_trump_tweets(spark)
+        self.biden_tweets = get_biden_tweets(spark)
         self.twitter_data.count()
+        self.trump_tweets.count()
+        self.biden_tweets.count()
 
 
 def get_twitter_data(spark):
@@ -32,7 +36,41 @@ def get_twitter_data(spark):
         .option("quote", "\"") \
         .option("sep", ",") \
         .csv(file_names, schema=schema).sample(fraction=SAMPLE_FRACTION, seed=SEED) \
-        .withColumn("hashtags", from_json(col("hashtags"), ArrayType(StringType())))
+        .withColumn("hashtags", from_json(col("hashtags"), ArrayType(StringType()))) \
+        .withColumn("mentions", from_json(col("mentions"), ArrayType(StringType())))
 
-    twitter_data = twitter_data.cache()
-    return twitter_data
+    return twitter_data.cache()
+
+
+def get_biden_tweets(spark):
+    file_names = [f"{DATASET_FOLDER}/biden/biden_{d}.csv" for d in range(DAY_START, DAY_END + 1)]
+
+    schema = StructType.fromJson(json.loads(open(SCHEMA_DIR).read()))
+    biden_tweets = spark.read \
+        .option("header", "true") \
+        .option("multiLine", "true") \
+        .option("nullValue", "NA") \
+        .option("escape", "\"") \
+        .option("quote", "\"") \
+        .option("sep", ",") \
+        .csv(file_names, schema=schema) \
+        .withColumn("hashtags", from_json(col("hashtags"), ArrayType(StringType()))) \
+        .withColumn("mentions", from_json(col("mentions"), ArrayType(StringType())))
+
+    return biden_tweets.cache()
+
+
+def get_trump_tweets(spark):
+    schema = StructType.fromJson(json.loads(open(SCHEMA_DIR).read()))
+    trump_tweets = spark.read \
+        .option("header", "true") \
+        .option("multiLine", "true") \
+        .option("nullValue", "NA") \
+        .option("escape", "\"") \
+        .option("quote", "\"") \
+        .option("sep", ",") \
+        .csv('dataset/trump/trump.csv', schema=schema) \
+        .withColumn("hashtags", from_json(col("hashtags"), ArrayType(StringType()))) \
+        .withColumn("mentions", from_json(col("mentions"), ArrayType(StringType())))
+
+    return trump_tweets.cache()
