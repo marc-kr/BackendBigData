3c830bb18de768ec28cb777f28734847bd0c0c24
marckr <decariamarco98@gmail.com>
2024-02-18 18:52:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ from spark_repository import SparkRepository
 #logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
-spark = SparkSession.builder\
+spark = SparkSession.builder \
+    .config('spark.driver.memory', '3g') \
+    .config('spark.executor.memory', '2g') \
     .config("spark.driver.bindAddress", "127.0.0.1")\
-    .config('spark.driver.memory', '3g')\
-    .config('spark.executor.memory', '2g')\
     .getOrCreate()
 dataset = Dataset(spark)
-spark_repository = SparkRepository(dataset.twitter_data, dataset.trump_tweets, dataset.biden_tweets)
+spark_repository = SparkRepository(dataset.twitter_data, dataset.trump_tweets, dataset.biden_tweets, dataset.sa_data)
 
 import controllers
 
